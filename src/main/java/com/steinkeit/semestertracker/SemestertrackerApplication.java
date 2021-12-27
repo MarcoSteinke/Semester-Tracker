5cd073aab34378f14b8389059a9931a37d511b80
maste150hhu <mastremolten2@gmail.com>
2021-12-27 10:16:29 +0100

    add spring project

---
+++ src/main/java/com/steinkeit/semestertracker/SemestertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.steinkeit.semestertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestertrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steinkeit/semestertracker/SemestertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steinkeit.semestertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
